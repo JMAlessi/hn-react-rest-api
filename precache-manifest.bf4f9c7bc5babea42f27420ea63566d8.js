5f461cd518ff13a6bf51ede7b8f0f5209582a8d2
J.M. Alessi <j.m.alessi1@outlook.com>
2020-01-28 23:40:49 -0500

    Updates

---
+++ precache-manifest.bf4f9c7bc5babea42f27420ea63566d8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84daf5ffd69584086684a93281510238",
+    "url": "/hn-react-app/index.html"
+  },
+  {
+    "revision": "46b7017fe04edb3007e8",
+    "url": "/hn-react-app/static/js/2.2930c8b0.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/hn-react-app/static/js/2.2930c8b0.chunk.js.LICENSE"
+  },
+  {
+    "revision": "72d08a729c83a2cf1cc0",
+    "url": "/hn-react-app/static/js/main.ecb5d4a3.chunk.js"
+  },
+  {
+    "revision": "5e427cd2ba19b7181a24",
+    "url": "/hn-react-app/static/js/runtime-main.d304b272.js"
+  }
+]);
