c206085c89cb47e2a50346b30e1262b0cc14a577
J. A. Software <46306007+J-A-Software@users.noreply.github.com>
2023-04-23 15:02:45 -0400

    refactor

--- src/App.js
+++ src/App.js
@@ -1,4 +1,22 @@
-import React from "react";
-import { StoriesContainer } from "./containers/StoriesContainer.js";
+import React from 'react';
+import { StoryService } from './services/StoryService.js';
+import { StoryList } from './components/StoryList.js';
 
-export const App = () => <StoriesContainer />;
+const storyService = new StoryService();
+
+export const App = () => {
+	const [stories, setStories] = React.useState([]);
+
+	React.useEffect(() => {
+		storyService.getTopStories().then((data) => {
+			setStories(data);
+		});
+	}, []);
+
+	return (
+		<div>
+			<h1>Top Stories</h1>
+			<StoryList stories={stories} />
+		</div>
+	);
+};
--- src/components/Story.js
+++ src/components/Story.js
@@ -1,36 +1,54 @@
-import React, { useState, useEffect, memo } from "react";
-import { getStory } from "../services/hnApi.js";
+import React, { useState, useEffect, memo } from 'react';
+import PropTypes from 'prop-types';
+import { getStory } from '../services/hnApi.js';
 import {
-  StoryWrapper,
-  StoryTitle,
-  StoryMeta,
-  StoryMetaElement
-} from "../styles/StoryStyles.js";
-import { mapTime } from "../mappers/mapTime.js";
+	StoryWrapper,
+	StoryTitle,
+	StoryMeta,
+	StoryMetaElement,
+} from '../styles/StoryStyles.js';
+import { mapTime } from '../mappers/mapTime.js';
 
-export const Story = memo(function Story({ storyId }) {
-  const [story, setStory] = useState({});
+function Story({ storyId }) {
+	const [story, setStory] = useState({});
 
-  useEffect(() => {
-    getStory(storyId).then(data => data && data.url && setStory(data));
-  }, [storyId]);
+	useEffect(() => {
+		async function fetchStory() {
+			const data = await getStory(storyId);
+			if (data && data.url) {
+				setStory(data);
+			}
+		}
+		fetchStory();
+	}, [storyId]);
 
-  return story && story.url ? (
-    <StoryWrapper data-testid="story">
-      <StoryTitle>
-        <a href={story.url}>{story.title}</a>
-      </StoryTitle>
+	return story && story.url ? (
+		<StoryWrapper data-testid="story">
+			<StoryTitle>
+				<a
+					href={story.url}
+					rel="noopener noreferrer"
+					target="_blank"
+				>
+					{story.title}
+				</a>
+			</StoryTitle>
+			<StoryMeta>
+				<span data-testid="story-by">
+					<StoryMetaElement color="#222">By: </StoryMetaElement>
+					{story.by}
+				</span>
+				<span data-testid="story-time">
+					<StoryMetaElement color="#222">Posted: </StoryMetaElement>
+					{mapTime(story.time)}
+				</span>
+			</StoryMeta>
+		</StoryWrapper>
+	) : null;
+}
 
-      <StoryMeta>
-        <span data-testid="story-by">
-          <StoryMetaElement color="#222">By: </StoryMetaElement>
-          {story.by}
-        </span>
-        <span data-testid="story-time">
-          <StoryMetaElement color="#222">Posted: </StoryMetaElement>
-          {mapTime(story.time)}
-        </span>
-      </StoryMeta>
-    </StoryWrapper>
-  ) : null;
-});
+Story.propTypes = {
+	storyId: PropTypes.number.isRequired,
+};
+
+export default memo(Story);
